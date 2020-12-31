f9ac5a1b2269341855e45938cc7ff5f346b62c76
AggrievedBubble <35272149+AggrievedBubble@users.noreply.github.com>
2020-12-31 01:45:35 +0000

    Initial commit

---
+++ src/main/java/com/jacobneave/safespectate/SafeSpectate.java
@@ -0,0 +1,16 @@
+package com.jacobneave.safespectate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SafeSpectate extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
